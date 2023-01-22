2672a00d7e700836eee012293a0af1a39f9e6fab
Mirco Forcella <forcella1996@gmail.com>
2023-01-22 01:03:13 +0100

    Initial commit

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,6 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        System.out.printf("%s %n" +
+                "The number Pi %f", "MIRCOOOOOOOO", 3.141590);
+    }
+}
